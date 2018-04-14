97c4b65b1e9f7db2c86cb6497a7556b2ac66fc19
almeila <gawe4507@gmail.com>
2018-04-14 19:30:57 +0900

    管理画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -15,9 +14,9 @@ class UsersController < ApplicationController
     end
   end
 
-private
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                :password_confirmation)
-  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   post 'task_status_update', to: 'tasks#status_update'
-
   resources :users
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  namespace :admin do
+    get '/', to: 'home#index'
+    resources :users do
+      resources :tasks, only: [:index]
+    end
+  end
 end
