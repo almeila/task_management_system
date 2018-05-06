e5f1f2c833d25b66e58f9193cca78a31a35ae9cb
almeila <gawe4507@gmail.com>
2018-05-06 10:45:32 +0900

    seed調整

--- db/seeds/first_user.rb
+++ db/seeds/first_user.rb
@@ -1,4 +1,4 @@
-user = User.create(name: '管理人A', email: 'testmail@first.com', password: 'password')
+user = User.create(name: '管理人A', email: 'testmail@first.com', password: 'password', admin: 1)
 
 Task.where(user_id: nil).each do |task|
   task.update_attribute(:user_id, user.id)
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,6 +1,7 @@
 10.times do
   User.create(
     name: Faker::Name.unique.name,
-    email: Faker::Internet.unique.email
+    email: Faker::Internet.unique.email,
+    password: 'password'
    )
 end
